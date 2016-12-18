6311c3f383f35392b4430f1d7f211b67b7fb3d1e
Christopher Kuzma <chris.kuzma@gmail.com>
2016-12-18 17:10:54 -0500

    Added a simple test program utilizing the Curie's accelerometer

---
+++ tap-flash/tap-flash.ino
@@ -0,0 +1,29 @@
+#include "CurieIMU.h"
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600); // initialize Serial communication
+  while(!Serial) ;    // wait for serial port to connect..
+  /* Initialise the IMU */
+  CurieIMU.begin();
+  CurieIMU.attachInterrupt(eventCallback);
+
+  /* Enable Shock Detection */
+  CurieIMU.setDetectionThreshold(CURIE_IMU_SHOCK, 1500); // 1.5g = 1500 mg
+  CurieIMU.setDetectionDuration(CURIE_IMU_SHOCK, 50);   // 50ms
+  CurieIMU.interrupts(CURIE_IMU_SHOCK);
+
+  Serial.println("IMU initialisation complete, waiting for events...");
+}
+
+void loop() {
+  // We don't need to put anything in the main loop...
+}
+
+static void eventCallback(void) {
+  if (CurieIMU.getInterruptStatus(CURIE_IMU_SHOCK)) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(50);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
