f3ace886bef95c88b75bfe8b294814f93bd1612c
Christopher Kuzma <chris.kuzma@gmail.com>
2016-12-21 21:04:52 -0800

    Implemented a weighted-average method to normalize tempo

--- dance-beat/dance-beat.ino
+++ dance-beat/dance-beat.ino
@@ -1,17 +1,19 @@
 #include "CurieIMU.h"
 
-/* --- Adjust variables BELOW this line --- */
-const int tapsToRemember  = 10; // Number of taps to store, which are averaged out
+/* --- Adjustable variables BELOW this line --- */
+const int tapsToRemember  = 5; // Number of taps to store, which are averaged out
 long delayTime            = 1000; // Initial setup delay for flashing, in milliseconds
-int shockThreshold        = 1050; // (DEFAULT: 1050mg)
-int shockDuration         = 125; // (DEFAULT: 125ms)
-/* --- Adjust variables ABOVE this line --- */
+int shockThreshold        = 1050; // (DEFAULT: 1050mg) Calibrate to installation
+int shockDuration         = 200; // (DEFAULT: 200ms) Calibrate to installation
+float oldBeatWeight       = 0.45; // Weight assigned to existing beat
+float newBeatWeight       = 0.55; // Weight assigned to new beat
+/* --- Adjustable variables ABOVE this line --- */
 
-/* These are used to measure taps */
+/* Below are variables used to measure time between taps */
 bool inTap = false;
 long tapStart;
 long tapEnd;
-long taps[tapsToRemember];
+long recentTaps[tapsToRemember];
 int currentTap = 0;
 
 void setup(){
@@ -23,7 +25,7 @@ void setup(){
 
   /* Fill the taps array */
   for (int x=0; x < tapsToRemember; x++){
-    taps[x] = delayTime;
+    recentTaps[x] = delayTime;
   }
   
   /* Initialise the IMU */
@@ -56,13 +58,23 @@ void pause(long pauseTime){
 }
 
 void calculateDelay(long newDelay){
-  taps[currentTap] = newDelay;
+  /* Add new delay measure to recent taps array */
+  recentTaps[currentTap] = newDelay;
+
+  /* Add all of the recent taps together */
   long tapAverage = 0;
   for (int x=0; x < tapsToRemember; x++){
-    tapAverage += taps[x];
+    tapAverage += recentTaps[x];
   }
-  delayTime = tapAverage / tapsToRemember;
+
+  /* Divide the recent taps to get their average */
+  tapAverage = tapAverage / tapsToRemember;
+
+  /* Give existing and new beats weights, add together */
+  delayTime = (oldBeatWeight * delayTime) + (newBeatWeight * tapAverage);
   Serial.println("Delay is now: " + String(delayTime));
+
+  /* Increment selector for adding next measurement to recent taps */
   currentTap++; // Increment the index selector
   if (currentTap == tapsToRemember){
     currentTap = 0;
@@ -83,7 +95,6 @@ static void eventCallback(void){
       inTap = true;
     }
     else{
-      
       tapEnd = millis();
       Serial.print(" and end. (" + String(tapEnd - tapStart) + ")\n");
       calculateDelay(tapEnd - tapStart);
