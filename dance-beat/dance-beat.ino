8493be954acfe29aba8b55b3f1b3cc7eae09e001
Christopher Kuzma <chris.kuzma@gmail.com>
2016-12-21 18:39:31 -0800

    Got it working, but need a better way to calculate averages

--- dance-beat/dance-beat.ino
+++ dance-beat/dance-beat.ino
@@ -1,24 +1,38 @@
 #include "CurieIMU.h"
 
-/* The global delay time for taps */
-long delayTime = 1000;
+/* --- Adjust variables BELOW this line --- */
+const int tapsToRemember  = 10; // Number of taps to store, which are averaged out
+long delayTime            = 1000; // Initial setup delay for flashing, in milliseconds
+int shockThreshold        = 1050; // (DEFAULT: 1050mg)
+int shockDuration         = 125; // (DEFAULT: 125ms)
+/* --- Adjust variables ABOVE this line --- */
 
 /* These are used to measure taps */
 bool inTap = false;
 long tapStart;
 long tapEnd;
+long taps[tapsToRemember];
+int currentTap = 0;
 
 void setup(){
+  /* Start serial for debugging */
+  Serial.begin(9600);
+  
   /* Set up the built-in LED */
   pinMode(LED_BUILTIN, OUTPUT);
+
+  /* Fill the taps array */
+  for (int x=0; x < tapsToRemember; x++){
+    taps[x] = delayTime;
+  }
   
   /* Initialise the IMU */
   CurieIMU.begin();
   CurieIMU.attachInterrupt(eventCallback);
 
   /* Enable Shock Detection */
-  CurieIMU.setDetectionThreshold(CURIE_IMU_SHOCK, 1050); // 1.050g = 1050mg
-  CurieIMU.setDetectionDuration(CURIE_IMU_SHOCK, 75);   // 75ms
+  CurieIMU.setDetectionThreshold(CURIE_IMU_SHOCK, shockThreshold);
+  CurieIMU.setDetectionDuration(CURIE_IMU_SHOCK, shockDuration);
   CurieIMU.interrupts(CURIE_IMU_SHOCK);
 }
 
@@ -27,18 +41,32 @@ void loop(){
 }
 
 void blinker(){
-  /* Flash on */
+  /* Flash */
   digitalWrite(LED_BUILTIN, HIGH);
-  pause(delayTime);
-
-  /* Flash off */
+  pause(50);
   digitalWrite(LED_BUILTIN, LOW);
+
+  /* Delay/Pause */
   pause(delayTime);
 }
 
 void pause(long pauseTime){
   unsigned long pauseStart = millis();
   while (millis() < pauseStart + pauseTime);
+}
+
+void calculateDelay(long newDelay){
+  taps[currentTap] = newDelay;
+  long tapAverage = 0;
+  for (int x=0; x < tapsToRemember; x++){
+    tapAverage += taps[x];
+  }
+  delayTime = tapAverage / tapsToRemember;
+  Serial.println("Delay is now: " + String(delayTime));
+  currentTap++; // Increment the index selector
+  if (currentTap == tapsToRemember){
+    currentTap = 0;
+  }
 }
 
 static void eventCallback(void){
@@ -50,12 +78,15 @@ static void eventCallback(void){
 
     /* Measure the tap length */
     if (inTap == false){
+      Serial.print("Tap start...");
       tapStart = millis();
       inTap = true;
     }
     else{
+      
       tapEnd = millis();
-      delayTime = tapEnd - tapStart;
+      Serial.print(" and end. (" + String(tapEnd - tapStart) + ")\n");
+      calculateDelay(tapEnd - tapStart);
       inTap = false;
     }
   }
