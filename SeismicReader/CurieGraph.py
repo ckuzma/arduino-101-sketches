32e0464f53a66294a2bc13ad1249af3637b2a731
Christopher Kuzma <chris.kuzma@gmail.com>
2017-01-04 16:18:23 -0800

    Updates for graph scale, added min/max
    Gyroscope ranges set to +/- 35,000
    Accelerometer ranges set to +/- 25,000
    Added min/max observed readings

--- SeismicReader/CurieGraph.py
+++ SeismicReader/CurieGraph.py
@@ -3,17 +3,48 @@ import os
 import serial
 import sys
 
+MAX = {
+    'ax': 0,
+    'ay': 0,
+    'az': 0,
+    'gx': 0,
+    'gy': 0,
+    'gz': 0
+}
+MIN = {
+    'ax': 0,
+    'ay': 0,
+    'az': 0,
+    'gx': 0,
+    'gy': 0,
+    'gz': 0
+}
+
 class CurieGrapher:
+    def calculate_min_max(self, input_data):
+        for key,value in input_data.items():
+            if value > MAX[key]:
+                MAX[key] = value
+            if value < MIN[key]:
+                MIN[key] = value
+
     def build_bar_graph(self, label, value, minimum, maximum):
         return label + '  ' + self.draw_bar(value, minimum, maximum)
 
     def graph_live_data(self, input_data):
-        output_string = self.build_bar_graph('ax (' + str(input_data['ax']) + ')\t', input_data['ax'], -20000, 20000) + '\n'
-        output_string += self.build_bar_graph('ay (' + str(input_data['ay']) + ')\t', input_data['ay'], -20000, 20000) + '\n'
-        output_string += self.build_bar_graph('az (' + str(input_data['az']) + ')\t', input_data['az'], -20000, 20000) + '\n'
-        output_string += self.build_bar_graph('gx (' + str(input_data['gx']) + ')\t', input_data['gx'], -20000, 20000) + '\n'
-        output_string += self.build_bar_graph('gy (' + str(input_data['gy']) + ')\t', input_data['gy'], -20000, 20000) + '\n'
-        output_string += self.build_bar_graph('gz (' + str(input_data['gz']) + ')\t', input_data['gz'], -20000, 20000)
+        self.calculate_min_max(input_data)
+        output_string = self.build_bar_graph('ax (' + str(input_data['ax']) + ')\t', input_data['ax'], -25000, 25000) + '\n'
+        output_string += self.build_bar_graph('ay (' + str(input_data['ay']) + ')\t', input_data['ay'], -25000, 25000) + '\n'
+        output_string += self.build_bar_graph('az (' + str(input_data['az']) + ')\t', input_data['az'], -25000, 25000) + '\n'
+        output_string += self.build_bar_graph('gx (' + str(input_data['gx']) + ')\t', input_data['gx'], -35000, 35000) + '\n'
+        output_string += self.build_bar_graph('gy (' + str(input_data['gy']) + ')\t', input_data['gy'], -35000, 35000) + '\n'
+        output_string += self.build_bar_graph('gz (' + str(input_data['gz']) + ')\t', input_data['gz'], -35000, 35000) + '\n\n'
+        output_string += 'ax\tMAX = ' + str(MAX['ax']) + '\tMIN = ' + str(MIN['ax']) + '\n'
+        output_string += 'ay\tMAX = ' + str(MAX['ay']) + '\tMIN = ' + str(MIN['ay']) + '\n'
+        output_string += 'az\tMAX = ' + str(MAX['az']) + '\tMIN = ' + str(MIN['az']) + '\n'
+        output_string += 'gx\tMAX = ' + str(MAX['gx']) + '\tMIN = ' + str(MIN['gx']) + '\n'
+        output_string += 'gy\tMAX = ' + str(MAX['gy']) + '\tMIN = ' + str(MIN['gy']) + '\n'
+        output_string += 'gz\tMAX = ' + str(MAX['gz']) + '\tMIN = ' + str(MIN['gz'])
         if sys.platform == 'win32':
             os.system('cls')
         else:
